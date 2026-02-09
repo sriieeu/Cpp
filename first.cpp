e158848f2e6f3e2d8b08e42e8d64a4a0726e24d7
sriieeu <sriieeuu@gmail.com>
2026-02-09 21:36:15 +0530

    namespace

--- first.cpp
+++ first.cpp
@@ -6,4 +6,6 @@ int main() {
     for (int i = 0 ; i < a; i++) {
         std::cout << i << std::endl;
     }
+    std::string z = "Hi";
+    std::cout<< z << std::endl;
 }
---
+++ namespace.cpp
@@ -0,0 +1,14 @@
+//
+// Created by SRI CHARAN on 09-02-2026.
+//
+#include <iostream>
+namespace first {
+    int x =0;
+}
+
+int main() {
+    int x=1;
+    std::cout<<x<<std::endl;
+    std::cout<< first::x<<std::endl;
+        return 0;
+}
