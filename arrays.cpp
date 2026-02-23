7e0ea3d7b09d2f274ac721893cb2509b4b36691c
sriieeu <sriieeuu@gmail.com>
2026-02-23 15:46:53 +0530

    array

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+//
+// Created by SRI CHARAN on 23-02-2026.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    string car[3];
+    car[0] = "A";
+    car[1] = "B";
+    car[2] = "C";
+    cout<<car[0]<<car[1]<<car[2]<<"\n";
+    cout<<sizeof(car);
+}
