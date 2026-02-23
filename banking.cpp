86e92c398bbd11ec81b167f53d49f3c8e75bad13
sriieeu <sriieeuu@gmail.com>
2026-02-23 11:39:07 +0530

    banking

--- banking.cpp
+++ banking.cpp
@@ -1,3 +1,57 @@
-//
-// Created by SRI CHARAN on 22-02-2026.
-//
+#include <iostream>
+using namespace std;
+
+void showBalance(double balance);
+void addBalance(double &balance);
+void withdrawal(double &balance);
+
+int main() {
+    double balance = 5000;
+    int choose;
+
+    cout << "1. Show Balance\n";
+    cout << "2. Add Balance\n";
+    cout << "3. Withdraw\n";
+    cout << "Enter choice: ";
+    cin >> choose;
+
+    switch (choose) {
+        case 1:
+            showBalance(balance);
+            break;
+        case 2:
+            addBalance(balance);
+            break;
+        case 3:
+            withdrawal(balance);
+            break;
+        default:
+            cout << "Invalid choice\n";
+    }
+
+    cout << "current balance: " << balance << endl;
+    return 0;
+}
+
+void showBalance(double balance) {
+
+}
+
+void addBalance(double &balance) {
+    double amount;
+    cout << "Enter amount to add: ";
+    cin >> amount;
+    balance += amount;
+}
+
+void withdrawal(double &balance) {
+    double amount;
+    cout << "Enter amount to withdraw: ";
+    cin >> amount;
+
+    if (amount > balance) {
+        cout << "Insufficient funds\n";
+    } else {
+        balance -= amount;
+    }
+}
